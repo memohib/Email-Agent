6daf5dc6a6b7b9d563bb1df9f887b6ddb0aa1cb6
Mohib Mansoory <memohib04@gmail.com>
2026-01-29 21:11:54 +0530

    feat: Refactor ai_service_tool to use environment variables for API keys and update dependencies

--- app/api/ai_service_tool.py
+++ app/api/ai_service_tool.py
@@ -1,15 +1,17 @@
 from langchain_groq import ChatGroq
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 def get_llm():
     model=ChatGroq(
         model = "openai/gpt-oss-120b",
-        api_key = "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",
+        api_key = os.getenv("GROQ_REASON_API_KEY"),
     )
     return model
 
 def get_structured_llm():
     model=ChatGroq(
         model = "meta-llama/llama-4-maverick-17b-128e-instruct",
-        api_key = "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",
+        api_key = os.getenv("GROQ_REASON_API_KEY"),
     )
     return model
